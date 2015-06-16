760f1a3ad30645cf533b5f0c4e8f5f81d1486e85
mahsompa <mahsompa@DIN34000338.corp.capgemini.com>
2015-06-16 16:38:57 +0530

    commit

---
+++ Question_8/src/FactorialDemo.java
@@ -0,0 +1,18 @@
+
+
+public class FactorialDemo {
+
+	public static void main(String[] args) {
+		int fact=1;
+	
+		int num=1;
+		while(num<=4)
+		{
+			fact=fact*num;
+			num++;
+		}
+		
+ System.out.println("fact"+fact);
+	}
+
+}
