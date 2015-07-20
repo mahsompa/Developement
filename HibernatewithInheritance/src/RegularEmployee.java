2dbc459e49eab01ffc585107df98f74132afb31f
mahsompa <mahsompa@DIN34002191.corp.capgemini.com>
2015-07-20 10:24:57 +0530

    Commited Single Table Strategy

---
+++ HibernatewithInheritance/src/RegularEmployee.java
@@ -0,0 +1,20 @@
+
+public class RegularEmployee extends Employee{
+	private int empsal;
+	private int empbonus;
+	public int getEmpsal() {
+		return empsal;
+	}
+	public void setEmpsal(int empsal) {
+		this.empsal = empsal;
+	}
+	public int getEmpbonus() {
+		return empbonus;
+	}
+	public void setEmpbonus(int empbonus) {
+		this.empbonus = empbonus;
+	}
+
+
+
+}
